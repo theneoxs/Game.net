9944011c467cdb1a1aa8dc7e7765b8333fbc59b7
Anton <anton_vasilev00@mail.ru>
2023-07-30 17:54:18 +0300

    убрал принты в скрипте

--- Players/Board.gd
+++ Players/Board.gd
@@ -1,12 +1,9 @@
 extends RigidBody2D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
@@ -15,9 +12,7 @@ var count_collision = 0
 func _on_body_entered(body):
 	if body.get_name() == "Player":
 		count_collision += 1
-		print("connected")
 		if count_collision >= 6:
-			print("пора рассыпаться")
 			queue_free()
 
 
