09c5d7da1a959daba6a305533bbf8fbb76fcaea9
Alexander Kamenev <kamenevv2@gmail.com>
2023-07-30 20:15:15 +0300

    Добавил время койота

--- Players/Player.gd
+++ Players/Player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -500.0
+var coyouteTime = 2.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -24,7 +25,7 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("m_jump") and is_on_floor():
+	if (Input.is_action_just_pressed("m_jump") and is_on_floor()) or (Input.is_action_just_pressed("m_jump") and coyouteTime >0):
 		velocity.y = JUMP_VELOCITY
 
 	var direction = Input.get_axis("m_left", "m_right")
@@ -33,6 +34,11 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	move_and_slide()
+	
+	if is_on_floor():
+		coyouteTime = 2.0
+	elif not is_on_floor():
+		coyouteTime -=0.1
 
 func update_animation_parameters():
 	if is_on_floor():
