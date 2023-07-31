170f77ce4ca55af4a998970e71fc1393a414993e
Anton <anton_vasilev00@mail.ru>
2023-07-31 16:47:50 +0300

    снял блок на события мыши

--- Players/Board.gd
+++ Players/Board.gd
@@ -17,7 +17,6 @@ var count_collision = 0
 func _on_body_entered(body):
 	if body.get_name() == "Player":
 		count_collision += 1
-		print(body, count_collision)
 		if count_collision >= 6:
 			queue_free()
 
