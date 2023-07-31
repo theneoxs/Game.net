6ff77697dff65f99b68509c398e6a76dc715188b
Artem <tema.makarov2012@gmail.com>
2023-07-31 20:44:20 +1000

    Add saw sprite

--- LevelObjects/CircleSaw.gd
+++ LevelObjects/CircleSaw.gd
@@ -14,4 +14,4 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	set_progress(get_progress() + slither_speed * delta)
-	pass
+	$SawSprite.rotation += delta*5
