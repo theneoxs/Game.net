d84edfb5c7fa37d3f3e2840cfe0feba761718b5a
Artem <tema.makarov2012@gmail.com>
2023-08-01 21:41:59 +1000

    Readd hero + add title screen on main menu

--- GUI/main_menu.gd
+++ GUI/main_menu.gd
@@ -1,11 +1,15 @@
 extends Control
 
 var is_change_scr = false
+@onready var main_text = $MainText
 
 func _ready():
 	Global.ready_screen()
 	Global.changing_scene.connect(change_scene_to)
 
+func _process(delta):
+	var mouse_pos = get_global_mouse_position()
+	main_text.modulate.a = (1-(Vector2(mouse_pos.x - 800, mouse_pos.y - 450).length())/200.0) * 0.5
 
 func _on_start_pressed():
 	Global.set_next_scr("res://GUI/game_start_screen.tscn")
--- Players/Player.gd
+++ Players/Player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -500.0
+var coyouteTime = 2.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -12,6 +13,8 @@ var walk_sprite
 var jump_sprite
 var death_sprite
 
+var onRope = false
+
 var is_death = false
 
 func _ready():
@@ -21,56 +24,86 @@ func _ready():
 	jump_sprite = $Jump
 	death_sprite = $Death
 	idle_sprite.show()
+	onRope = false
 
-func _process(delta):
-	update_animation_parameters()
+func kill_player():
+	print("Received custom signal")
 
 func _physics_process(delta):
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	update_animation_parameters()
+	if !is_death:
+		if onRope:
+			var Vdirection = Input.get_axis("m_up", "m_down")
+			if Vdirection:
+				velocity.y = Vdirection * SPEED
+			else:
+				velocity.y = move_toward(velocity.y, 0, SPEED)
+		
+		if not is_on_floor() and !onRope:
+			velocity.y += gravity * delta
+			
+		# Handle Jump.
+		if (Input.is_action_just_pressed("m_jump") and is_on_floor()) or (Input.is_action_just_pressed("m_jump") and coyouteTime >0):
+			velocity.y = JUMP_VELOCITY
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("m_jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	var direction = Input.get_axis("m_left", "m_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		var direction = Input.get_axis("m_left", "m_right")
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 	move_and_slide()
+
+	if is_on_floor():
+		coyouteTime = 2.0
+	elif not is_on_floor():
+		coyouteTime -=0.1
 
 func update_animation_parameters():
 	if is_death:
 		set_anim_tree(false, false, false, true)
 	elif is_on_floor():
 		if velocity == Vector2.ZERO:
-			set_anim_tree(true, false, false)
+			set_anim_tree(true, false, false, false)
 		else:
-			set_anim_tree(false, true, false)
+			set_anim_tree(false, true, false, false)
 	else:
-		set_anim_tree(false, false, true)
+		set_anim_tree(false, false, true, false)
 	
-	if velocity.x > 0 and !is_death:
-		flip_anim(false)
-	elif velocity.x < 0 and !is_death:
-		flip_anim(true)
+	if !is_death:
+		if Input.is_action_just_pressed("m_right"):
+			flip_anim(false)
+		elif Input.is_action_just_pressed("m_left"):
+			flip_anim(true)
 
 #Установка анимаций: idle, move и jump
-func set_anim_tree(is_idle : bool, is_moving : bool, is_jumpong : bool, is_death : bool = false):
+func set_anim_tree(is_idle : bool, is_moving : bool, is_jumping : bool, is_dying: bool):
 	animation_tree["parameters/conditions/idle"] = is_idle
 	animation_tree["parameters/conditions/is_moving"] = is_moving
-	animation_tree["parameters/conditions/is_jumping"] = is_jumpong
-	animation_tree["parameters/conditions/is_death"] = is_death
+	animation_tree["parameters/conditions/is_jumping"] = is_jumping
+	animation_tree["parameters/conditions/is_death"] = is_dying
 	
 	idle_sprite.visible = is_idle 
 	walk_sprite.visible = is_moving 
-	jump_sprite.visible = is_jumpong 
-	death_sprite.visible = is_death 
-	
+	jump_sprite.visible = is_jumping
+	death_sprite.visible = is_dying 
 
 func flip_anim(is_left):
 	idle_sprite.flip_h = is_left
 	walk_sprite.flip_h = is_left
 	jump_sprite.flip_h = is_left
+	death_sprite.flip_h = is_left
 	walk_sprite.offset.x = -10 if is_left else 10
+
+
+
+func _on_area_2d_body_entered(body):
+	set_collision_mask_value(1, false)
+	onRope = true
+	print('enter')
+
+
+func _on_area_2d_body_exited(body):
+	set_collision_mask_value(1, true)
+	onRope = false
+	print('leave')
+
