f5567ffd2f6d97ab3d57ef95acc2533a9d391cb2
Alexander Kamenev <75051539+Nraj10@users.noreply.github.com>
2023-08-02 21:44:34 +0300

    Добавил хоррор на юните

---
+++ Levels/Level4.gd
@@ -0,0 +1,37 @@
+extends Control
+
+var block = load("res://Players/Block.tscn")
+var board = load("res://Players/Board.tscn")
+var fan = load("res://Players/Fan.tscn")
+@onready var spawnpoint = $Point_spawn
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Global.in_game = true
+	Global.ready_screen()
+	Global.changing_scene.connect(change_scene_to)
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func change_scene_to(scene = Global.next_scr):
+	get_tree().change_scene_to_file(scene)
+
+
+func _on_choose_mode_choosing_item(num):
+	if num == 0:
+		var spawn_block = block.instantiate()
+		spawn_block.position = spawnpoint.position
+		add_child(spawn_block)
+	
+	elif num == 1:
+		var spawn_block = board.instantiate()
+		spawn_block.position = spawnpoint.position
+		add_child(spawn_block)
+		
+	elif num == 2:
+		var spawn_block = fan.instantiate()
+		spawn_block.position = spawnpoint.position
+		add_child(spawn_block)
