f9cfa785e5041756ce06ad8ccdbfebe782620457
Artem <tema.makarov2012@gmail.com>
2023-08-03 16:43:00 +1000

    Add send logic

--- GUI/final.gd
+++ GUI/final.gd
@@ -1,10 +1,31 @@
 extends Control
 
+@onready var time_stamp = $TimeHeader/TimeStamp
+@onready var name_stamp = $NameHeader/NameStamp
+
+@onready var httpRequest = $HTTPRequest
+
+var url_server = ""
+
 func _ready():
 	Global.ready_screen()
 	Global.changing_scene.connect(change_scene_to)
-	$TimeHeader/TimeStamp.text = Global.calc_complete_time(Global.time1 + Global.time2 +Global.time3 +Global.time4 +Global.time5 )
-
+	time_stamp.text = Global.calc_complete_time(Global.time1 + Global.time2 +Global.time3 +Global.time4 +Global.time5 )
+	var hashname = str(hash("%.2f %.2f %.2f %.2f %.2f" % [Global.time1, Global.time2, Global.time3, Global.time4, Global.time5])/10)
+	name_stamp.text = hashname.sha256_text().substr(0, 10)
+	var result = {
+		"token" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
+		"data" : {
+			"name" : name_stamp.text,
+			"x" : Global.time1,
+			"y" : Global.time2,
+			"z" : Global.time3,
+			"a" : Global.time4,
+			"b" : Global.time5
+		}
+	}
+	send_HTTPRequest(url_server, result)
+	
 
 func _on_next_pressed():
 	_on_btn_click()
@@ -19,3 +40,11 @@ func _on_start_mouse_entered():
 
 func _on_btn_click():
 	Global.play_btn_click()
+
+func send_HTTPRequest(url, data, use_ssl = false):
+	var query = JSON.stringify(data)
+	var headers = ["Content-Type: application/json"]
+	httpRequest.request(url, headers, HTTPClient.METHOD_POST, query)
+
+func _on_http_request_request_completed(result, response_code, headers, body):
+	print(result)
